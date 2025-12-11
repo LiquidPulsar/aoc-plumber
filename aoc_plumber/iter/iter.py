45642460a7977e62a9ee88a7a880371f83968f1c
Alex <noelalexandre66@gmail.com>
2025-12-11 04:17:51 +0000

    Drop 3.12 syntax for compat

--- aoc_plumber/iter/iter.py
+++ aoc_plumber/iter/iter.py
@@ -1,9 +1,12 @@
 import functools
 import itertools
-from typing import Iterator, Callable, Self
+from typing import Iterator, Callable, TypeVar
+
+U = TypeVar("U")
+T = TypeVar("T")
 
 
-class Iter[T](Iterator[T]):
+class Iter(Iterator[T]):
     __slots__ = ("iterable",)
 
     def __init__(self, iterable: Iterator[T]):
@@ -15,17 +18,17 @@ class Iter[T](Iterator[T]):
     def __next__(self) -> T:
         return next(self.iterable)
 
-    def filter(self, func) -> Self:
+    def filter(self, func: Callable[[T], bool]) -> "Iter[T]":
         self.iterable = filter(func, self.iterable)
         return self
 
-    def map[U](self, func: Callable[[T], U]) -> "Iter[U]":
+    def map(self, func: Callable[[T], U]) -> "Iter[U]":
         return Iter(map(func, self.iterable))
 
-    def flat_map[U](self, func: Callable[[T], Iterator[U]]) -> "Iter[U]":
+    def flat_map(self, func: Callable[[T], Iterator[U]]) -> "Iter[U]":
         return Iter(y for x in self.iterable for y in func(x))
 
-    def collect[U](self, t: Callable[[Iterator[T]], U] = list) -> U:
+    def collect(self, t: Callable[[Iterator[T]], U] = list) -> U:
         return t(self.iterable)
 
     def any(self, func: Callable[[T], bool]) -> bool:
@@ -49,23 +52,23 @@ class Iter[T](Iterator[T]):
     def enumerate(self, start: int = 0) -> "Iter[tuple[int, T]]":
         return Iter(enumerate(self.iterable, start=start))
 
-    def skip(self, n: int) -> Self:
+    def skip(self, n: int) -> "Iter[T]":
         for _ in range(n):
             next(self.iterable, None)
         return self
 
-    def take(self, n: int) -> Self:
+    def take(self, n: int) -> "Iter[T]":
         self.iterable = itertools.islice(self.iterable, n)
         return self
 
-    def take_while(self, func: Callable[[T], bool]) -> Self:
+    def take_while(self, func: Callable[[T], bool]) -> "Iter[T]":
         self.iterable = itertools.takewhile(func, self.iterable)
         return self
 
-    def drop_while(self, func: Callable[[T], bool]) -> Self:
+    def drop_while(self, func: Callable[[T], bool]) -> "Iter[T]":
         self.iterable = itertools.dropwhile(func, self.iterable)
         return self
 
-    def chain(self, other: Iterator[T]) -> Self:
+    def chain(self, other: Iterator[T]) -> "Iter[T]":
         self.iterable = itertools.chain(self.iterable, other)
         return self
